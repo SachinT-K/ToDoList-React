243d3a64858717e35373bdbe2ef75a2d23107f4a
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-06 13:39:26 +0530

    Added Comments.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState("");    // Hook for tracking input state change.
+  const [items, setItems] = useState([]);            // Hook for tracking list items state change.
 
   function handleChange(event) {
-    const newValue = event.target.value;
+    const newValue = event.target.value;  // holding added input value.//
     setInputText(newValue);
   }
 
   function handleClick() {
     setItems((prevItems) => {
-      return [...prevItems, inputText];
+      return [...prevItems, inputText];            //Spread Operator which adds previous items and newly added item to items array.
     });
-    setInputText("");
+    setInputText("");     //set input text to empty once entry to list is done
   }
 
   return (
@@ -23,15 +23,15 @@ function App() {
       </div>
 
       <div className="form">
-        <input type="text" onChange={handleChange} value={inputText} />
+        <input type="text" onChange={handleChange} value={inputText} />   //call function when input value adds
 
-        <button onClick={handleClick} type="submit">
+        <button onClick={handleClick} type="submit">  //call function when button is clicked.
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => {
+          {items.map((todoItem) => {            // Dive into items array and <li> created for each item(todoItem) inside it using Arrow function.
             return <li>{todoItem}</li>;
           })}
         </ul>
