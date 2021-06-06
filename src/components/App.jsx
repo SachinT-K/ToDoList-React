fe19eaf25df2784e895d42e474687912726ffc93
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-06 15:39:40 +0530

    Set strikee through for item when clicked on it.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
-  const [inputText, setInputText] = useState("");    // Hook for tracking input state change.
-  const [items, setItems] = useState([]);            // Hook for tracking list items state change.
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    const newValue = event.target.value;  // holding added input value.//
+    const newValue = event.target.value;
     setInputText(newValue);
   }
 
-  function handleClick() {
+  function addItem() {
     setItems((prevItems) => {
-      return [...prevItems, inputText];            //Spread Operator which adds previous items and newly added item to items array.
+      return [...prevItems, inputText];
     });
-    setInputText("");     //set input text to empty once entry to list is done
+    setInputText("");
   }
 
   return (
@@ -23,17 +24,17 @@ function App() {
       </div>
 
       <div className="form">
-        <input type="text" onChange={handleChange} value={inputText} />   //call function when input value adds
+        <input type="text" onChange={handleChange} value={inputText} />
 
-        <button onClick={handleClick} type="submit">  //call function when button is clicked.
+        <button onClick={addItem} type="submit">
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => {            // Dive into items array and <li> created for each item(todoItem) inside it using Arrow function.
-            return <li>{todoItem}</li>;
-          })}
+          {items.map((todoItem) => (
+            <ToDoItem text={todoItem} />
+          ))}
         </ul>
       </div>
     </div>
